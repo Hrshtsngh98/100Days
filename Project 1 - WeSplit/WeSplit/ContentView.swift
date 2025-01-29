fe467366e3dda73adcfd80e10f94f1e4a52e8792
Harshit Singh <harshit.singh@angi.com>
2025-01-29 10:55:39 -0500

    Project 1 - part 3 complete

--- Project 1 - WeSplit/WeSplit/ContentView.swift
+++ Project 1 - WeSplit/WeSplit/ContentView.swift
@@ -14,14 +14,18 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
-    private let tipPercentages: [Int] = [10, 15, 20, 25, 30, 0]
+    private let tipPercentages1: [Int] = [10, 15, 20, 25, 30, 0]
+    private let tipPercentages2: [Int] = Array(0..<101)
     
-    var totalPerPerson: Double {
-        var peopleCount = Double(numberOfPeople) + 2
+    var totalAmount: Double {
         let tipAmount = checkAmount * Double(tipPercentage) / 100
         let totalWithTip = checkAmount + tipAmount
-        
-        return totalWithTip / Double(peopleCount)
+        return totalWithTip
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) + 2
+        return totalAmount / Double(peopleCount)
     }
 
     var body: some View {
@@ -40,16 +44,31 @@ struct ContentView: View {
                     .pickerStyle(.navigationLink)
                 }
                 
+                // Part 2
+                // Section("How much do you want to tip?") {
+                //     Picker("Tip percentage", selection: $tipPercentage) {
+                //         ForEach(tipPercentages1, id: \.self) {
+                //             Text($0, format: .percent)
+                //         }
+                //     }
+                //     .pickerStyle(.segmented)
+                // }
+                
+                // Part 3
                 Section("How much do you want to tip?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(tipPercentages2, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
                 
-                Section {
+                Section("Total Amount") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
