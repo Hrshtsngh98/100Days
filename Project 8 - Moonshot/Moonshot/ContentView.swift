cfc1818cf6155c8b2d28af44b5ce1cf91f5b409e
Harshit Singh <harshit.singh@angi.com>
2025-02-28 13:11:08 -0500

    Project 8 - part 1 complete

---
+++ Project 8 - Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Harshit Singh on 2/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(.example)
+            .resizable()
+            .scaledToFit()
+            .containerRelativeFrame(.horizontal) { size, axis in
+                size * 0.8
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project 8 - Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Harshit Singh on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
