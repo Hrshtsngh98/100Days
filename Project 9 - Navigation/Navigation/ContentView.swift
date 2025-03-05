be698ec4ae7ab91504fae49afabd531ad2cb7635
Harshit Singh <harshit.singh@angi.com>
2025-03-05 16:00:32 -0500

    Project 9 - part 1 complete

---
+++ Project 9 - Navigation/Navigation/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Harshit Singh on 3/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            NavigationLink("Tap me") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project 9 - Navigation/Navigation/TutorialView.swift
@@ -0,0 +1,34 @@
+//
+//  TutorialView.swift
+//  Navigation
+//
+//  Created by Harshit Singh on 3/5/25.
+//
+
+import SwiftUI
+
+struct Student: Identifiable, Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
+struct TutorialView: View {
+    let students = [Student(name: "Harry", age: 25), Student(name: "Ron", age: 24), Student(name: "Hermione", age: 23)]
+    
+    var body: some View {
+        NavigationStack {
+            ForEach(students) { student in
+                NavigationLink("\(student.name)", value: student)
+                    .navigationDestination(for: Student.self) { student in
+                        Text(student.name)
+                    }
+                    .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    TutorialView()
+}
