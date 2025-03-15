3816adb216f13ed2f57b2c9ffac923c2e5b3d835
Harshit Singh <harshit.singh@angi.com>
2025-03-14 23:12:00 -0400

    Project 10 - part 2 complete

---
+++ Project 10 - CupcakeCorner/CupcakeCorner/TutorialView4.swift
@@ -0,0 +1,109 @@
+//
+//  TutorialView4.swift
+//  CupcakeCorner
+//
+//  Created by Harshit Singh on 3/14/25.
+//
+
+import CoreHaptics
+import SwiftUI
+
+struct TutorialView4: View {
+    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
+
+    var body: some View {
+        VStack(spacing: 40) {
+            Button("Tap Count: \(counter)") {
+                counter += 1
+            }
+            .sensoryFeedback(.success, trigger: counter)
+            //            .sensoryFeedback(.impact(weight: .light, intensity: 1), trigger: counter)
+            //            .sensoryFeedback(.increase, trigger: counter)
+            
+            Button("Custom success") {
+                complexSuccess()
+            }
+            
+            Button("Custom success Pattern 2") {
+                complexSuccessPattern2()
+            }
+
+
+        }
+        .onAppear {
+            prepareHaptics()
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
+    func complexSuccessPattern2() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
+}
+
+#Preview {
+    TutorialView4()
+}
