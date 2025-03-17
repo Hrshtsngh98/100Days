00fdb8ff2249477ed5c82e3b56aba99c9780d9f2
Harshit Singh <harshit.singh@angi.com>
2025-03-17 16:11:51 -0400

    Project 10 - part 3 complete

--- Project 10 - CupcakeCorner/CupcakeCorner/Order.swift
+++ Project 10 - CupcakeCorner/CupcakeCorner/Order.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 @Observable
-class Order {
+class Order: Codable {
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
-
+    
     var type = 0
     var quantity = 3
-
+    
     var specialRequestEnabled = false {
         didSet {
             if specialRequestEnabled == false {
@@ -24,4 +24,51 @@ class Order {
     }
     var extraFrosting = false
     var addSprinkles = false
+    
+    var name = ""
+    var city = ""
+    var streetAddress = ""
+    var zip = ""
+    
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty ||
+            streetAddress.isEmpty ||
+            city.isEmpty ||
+            zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+
+        // complicated cakes cost more
+        cost += Decimal(type) / 2
+
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+
+        return cost
+    }
 }
