12143685b6dd4eb30f9196126746a95158e850a3
Harshit Singh <harshit.singh@angi.com>
2025-03-25 14:06:14 +0530

    Project 11 - part 3 complete

--- Project 11 - Bookworm/Bookworm/App/EmojiRatingView.swift
+++ Project 11 - Bookworm/Bookworm/App/EmojiRatingView.swift
@@ -13,15 +13,15 @@ struct EmojiRatingView: View {
     var body: some View {
         switch rating {
         case 1:
-            Text("♥️")
+            Text("👎")
         case 2:
-            Text("😁")
+            Text("😞")
         case 3:
             Text("😑")
         case 4:
-            Text("😞")
+            Text("😁")
         default:
-            Text("👎")
+            Text("♥️")
         }
     }
 }
--- Project 11 - Bookworm/Bookworm/ContentView.swift
+++ Project 11 - Bookworm/Bookworm/ContentView.swift
@@ -10,7 +10,17 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+//    @Query var books: [Book]
+//    @Query(sort: \Book.title) var books: [Book]
+//    @Query(sort: \Book.title, order: .reverse) var books: [Book]
+//    @Query(sort: \Book.rating, order: .reverse) var books: [Book]
+//    @Query(sort: [SortDescriptor(\Book.title)]) var books: [Book]
+//    @Query(sort: [SortDescriptor(\Book.title, order: .reverse)]) var books: [Book]
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author)
+    ]) var books: [Book]
+
 
     @State private var showingAddScreen = false
     
@@ -32,19 +42,37 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .sheet(isPresented: $showingAddScreen) {
                 AddBookView()
             }
             .navigationTitle("Bookworm")
+            .navigationDestination(for: Book.self) { book in
+                BookDetailView(book: book)
+            }
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Book", systemImage: "plus") {
                         showingAddScreen.toggle()
                     }
                 }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
             }
        }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our query
+            let book = books[offset]
+
+            // delete it from the context
+            modelContext.delete(book)
+        }
     }
 }
 
