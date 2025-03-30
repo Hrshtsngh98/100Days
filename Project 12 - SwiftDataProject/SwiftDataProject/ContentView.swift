2b1d2883b9311a16a2d67abaa63dbc9b8da6f9a9
Harshit Singh <harshit.singh@angi.com>
2025-03-30 22:51:17 +0530

    Project 12 - part 2 complete

--- Project 12 - SwiftDataProject/SwiftDataProject/ContentView.swift
+++ Project 12 - SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -14,20 +14,20 @@ struct ContentView: View {
     
     //    @Query(sort: \User.name) var users: [User]
     
-//        @Query(filter: #Predicate<User> { user in
-//            user.name.contains("R")
-//        }, sort: \User.name) var users: [User]
+    //        @Query(filter: #Predicate<User> { user in
+    //            user.name.contains("R")
+    //        }, sort: \User.name) var users: [User]
     
-//    // For case ignore for "R"
-//    @Query(filter: #Predicate<User> { user in
-//        user.name.localizedStandardContains("R")
-//    }, sort: \User.name) var users: [User]
+    //    // For case ignore for "R"
+    //    @Query(filter: #Predicate<User> { user in
+    //        user.name.localizedStandardContains("R")
+    //    }, sort: \User.name) var users: [User]
     
     
-//    @Query(filter: #Predicate<User> { user in
-//        user.name.localizedStandardContains("R") &&
-//        user.city == "London"
-//    }, sort: \User.name) var users: [User]
+    //    @Query(filter: #Predicate<User> { user in
+    //        user.name.localizedStandardContains("R") &&
+    //        user.city == "London"
+    //    }, sort: \User.name) var users: [User]
     
     // Can write this like this
     @Query(filter: #Predicate<User> { user in
@@ -43,17 +43,19 @@ struct ContentView: View {
         }
     }, sort: \User.name) var users: [User]
     
+    @State private var showingUpcomingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate),
+    ]
+    
     var body: some View {
         NavigationStack(path: $path) {
-            List(users) { user in
-                NavigationLink(value: user) {
-                    Text(user.name)
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
+                .navigationTitle("Users")
+                .navigationDestination(for: User.self) { user in
+                    EditUserView(user: user)
                 }
-            }
-            .navigationTitle("Users")
-            .navigationDestination(for: User.self) { user in
-                EditUserView(user: user)
-            }
             .toolbar {
                 Button("Add User", systemImage: "plus") {
                     let user = User(name: "", city: "", joinDate: .now)
@@ -63,16 +65,36 @@ struct ContentView: View {
                 
                 Button("Add Samples", systemImage: "plus.square.on.square") {
                     try? modelContext.delete(model: User.self)
-
+                    
                     let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
                     let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
                     let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
                     let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
-
+                    
                     modelContext.insert(first)
                     modelContext.insert(second)
                     modelContext.insert(third)
                     modelContext.insert(fourth)
+                }
+                
+                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                    showingUpcomingOnly.toggle()
+                }
+                
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate),
+                            ])
+                        
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
                 }
             }
         }
