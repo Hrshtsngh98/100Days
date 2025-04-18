8d594c7d78ac0d14c6831b6df97a289b8475544e
Harshit-HomeAdvisor <harshit.singh@angi.com>
2025-04-18 19:01:14 +0530

    Project 15 - part 2 complete

--- Project 2 - GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project 2 - GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -24,6 +24,20 @@ struct ContentView: View {
     
     @State private var tappedFlagNumber: Int = -1
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     let totalGames = 5
     
     var body: some View {
@@ -68,6 +82,9 @@ struct ContentView: View {
                                     flagTapped(number)
                                 }
                             }
+                            .accessibilityLabel(labels[countries[number]] ?? "Unknown flag")
+                            .accessibilityElement(children: .ignore)
+                            .accessibilityAddTraits(.isButton)
                             .rotation3DEffect(.degrees(tappedFlagNumber == number ? rotationDegrees : 0), axis: (0,1,0))
                             .opacity(tappedFlagNumber == number ? 1 : wrongAnswerOpacity)
                             .scaleEffect(tappedFlagNumber == number ? 1 : wrongAnswerScale)
--- Project 5 - WordScramble/WordScramble/ContentView.swift
+++ Project 5 - WordScramble/WordScramble/ContentView.swift
@@ -38,6 +38,10 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+//                        .accessibilityLabel("\(word), \(word.count) letters")
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 }
             }
