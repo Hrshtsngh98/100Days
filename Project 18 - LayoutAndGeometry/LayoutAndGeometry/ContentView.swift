177b58ffe18be63b5da2a1b7437d0ee2a596cf62
Harshit-HomeAdvisor <harshit.singh@angi.com>
2025-05-16 16:22:11 -0400

    Project 18 - part 3 complete

--- Project 18 - LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Project 18 - LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                Color(
+                                    hue: min(1, Double.random(in: 0...1)),
+                                    saturation: 1,
+                                    brightness: 1
+                                )
+                            )
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(Double(1 - (200 - proxy.frame(in: .global).minY) / 200))
+                            .scaleEffect(
+                                max(0.5, Double(proxy.frame(in: .global).midY / fullView.frame(in: .global).height))
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
     }
 }
 
